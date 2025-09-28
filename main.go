d9a9a412445b36b6d3bd48b8c867db8a64c7329f
Ng1n3 <code.with.muyiwa@gmail.com>
2025-09-28 15:14:01 +0000

    feat: setup kaban process ui

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type status int
+
+const divisor = 4
+
 
 const (
 	todo status = iota
@@ -23,63 +27,94 @@ type Task struct {
 }
 
 // implement the list.Item interface
-func(t Task) FilterValue() string {
-  return t.title
+func (t Task) FilterValue() string {
+	return t.title
 }
 
 func (t Task) Title() string {
-  return t.title
+	return t.title
 }
 
 func (t Task) Description() string {
-  return t.description
+	return t.description
 }
 
 /* Main Model */
 
 type Model struct {
-  list list.Model
-  err error
+	focused status
+	lists   []list.Model
+	err     error
+  loaded bool
 }
 
 func New() *Model {
-  return &Model{}
+	return &Model{}
 }
 
-//TODO: call this on tea.windowsizemsg
-func (m *Model) initList(width, height int) {
-  m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
-  m.list.Title = "To Do"
-  m.list.SetItems([]list.Item{
-    Task{status: todo, title: "buy milk", description: "strawberry milk"},
-    Task{status: todo, title: "eat sushi", description: "negitoro roll, miso soup, rice"},
-    Task{status: todo, title: "fold laundry", description: "or wear wrinkled tshirt"},
-  })
+// TODO: call this on tea.windowsizemsg
+func (m *Model) initLists(width, height int) {
+	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor, height)
+	m.lists = []list.Model{defaultList, defaultList, defaultList}
+
+	// Init To Do
+	m.lists[todo].Title = "To Do"
+	m.lists[todo].SetItems([]list.Item{
+		Task{status: todo, title: "buy milk", description: "strawberry milk"},
+		Task{status: todo, title: "eat sushi", description: "negitoro roll, miso soup, rice"},
+		Task{status: todo, title: "fold laundry", description: "or wear wrinkled tshirt"},
+	})
+
+	// Init in Progress
+	m.lists[inProgress].Title = "In Progress"
+	m.lists[inProgress].SetItems([]list.Item{
+		Task{status: todo, title: "REST API", description: "building a REST API with Go"},
+		Task{status: todo, title: "Kafka", description: "Working with KafKa"},
+	})
+
+	m.lists[done].Title = "Done"
+	m.lists[done].SetItems([]list.Item{
+		Task{status: todo, title: "Kaban Board", description: "Building a Kaban board with Go"},
+	})
 }
 
 func (m Model) Init() tea.Cmd {
-  return nil
+	return nil
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-  switch msg := msg.(type) {
-  case tea.WindowSizeMsg:
-    m.initList(msg.Width, msg.Height)
-  }
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		if !m.loaded {
+      m.loaded = true
+    }
+    m.initLists(msg.Width, msg.Height)
+	}
 
-  var cmd tea.Cmd
-  m.list, cmd =  m.list.Update(msg)
-  return m, cmd
+	var cmd tea.Cmd
+	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
+	return m, cmd
 }
 
 func (m Model) View() string {
-  return m.list.View()
+  if m.loaded {
+	return lipgloss.JoinHorizontal(
+		lipgloss.Left,
+		m.lists[todo].View(),
+		m.lists[inProgress].View(),
+		m.lists[done].View(),
+	)
+  }else {
+   return "loading..."
+  }
+
 }
 
 func main() {
-  m := New()
-  p := tea.NewProgram(m)
-  if _, err := p.Run(); err != nil { fmt.Println(err)
-    os.Exit(1)
-  }
+	m := New()
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
